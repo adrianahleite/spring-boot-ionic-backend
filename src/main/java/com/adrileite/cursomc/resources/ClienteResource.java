a1c2e4f974286dff57b14c9d6ce075b4516d8137
Adriana Leite <drica_hleite@hotmail.com>
2020-04-24 09:50:36 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/adrileite/cursomc/resources/ClienteResource.java
@@ -0,0 +1,27 @@
+package com.adrileite.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adrileite.cursomc.domain.Cliente;
+import com.adrileite.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes") // end point
+public class ClienteResource {
+
+	@Autowired
+	private ClienteService service;
+
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity <?> find(@PathVariable Integer id) {
+
+		Cliente obj = service.find(id);
+		return ResponseEntity.ok().body(obj);   //1 teste
+	}
+
+}
